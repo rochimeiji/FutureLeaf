3a129a0fbf17a9971104db1550ac66e593fb6d9e
rochimeiji <rochim.eiji@yahoo.com>
2013-10-08 06:15:30 +0700

    config baseUrl

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/futureLeaf/';
+$config['base_url']	= 'http://localhost/FutureLeaf/';
 
 /*
 |--------------------------------------------------------------------------
